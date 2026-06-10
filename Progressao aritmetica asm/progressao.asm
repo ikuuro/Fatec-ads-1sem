df61c11d15a3477b24c47cb6288b13e7cd1379ee
ikuuro <105460647+ikuuro@users.noreply.github.com>
2026-06-09 22:51:01 -0300

    trabalho aoc finalizado

--- Progressao aritmetica asm/progressao.asm
+++ Progressao aritmetica asm/progressao.asm
@@ -26,3 +26,84 @@ syscall
 
 move $t0, $v0
 
+#verifica se n <= 0
+
+blez $t0, ler_n
+
+
+#inicializadores
+
+
+li $t1, 1    #contador i
+li $t2, 0    #H
+
+loop:
+
+#se i > n ->fim
+bgt $t1, $t0, fim
+
+
+
+
+#par ou impar
+
+
+#divide i por 2
+
+li $t3, 2
+div $t1, $t3
+
+#pega o resto da divisao 
+wfhi $t4
+
+
+
+#se o resto for =0 ->par
+beq $t4, $zero, par
+
+
+
+#impar -> H=H + (1*i)
+
+
+
+add $t2, $t2, $t1
+
+j continua
+
+par:
+
+
+#par -> H=H - (2*i)
+
+
+mul $t5, $t1 , 2
+
+sub $t2, $t2, $t5
+
+continua:
+
+#contador i++
+addi $t1, $t1, 1
+
+j loop
+
+fim:
+
+
+
+#resultado
+
+
+li $v0, 4
+la $a0, msg2
+syscall
+
+li $v0,1
+move $a0, $t2
+syscall
+
+#encerrar programa
+li$v0,10
+syscall
+
